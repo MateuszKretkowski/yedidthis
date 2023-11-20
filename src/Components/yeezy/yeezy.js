8c1381e01c1d2aefbf5086d3f1561a2160a809d7
MateuszKretkowski <mkretkowski13@gmail.com>
2023-11-20 18:08:00 +0100

    added working modals, text on yeezy

--- src/Components/yeezy/yeezy.js
+++ src/Components/yeezy/yeezy.js
@@ -1,99 +1,99 @@
 import "../../App.css";
 import "./yeezy.css";
-import "../../data/yeezyPhotos.js";
+import "../../data/yeezyDayNews.js";
 import React, { useState } from "react";
 import { motion, AnimatePresence, animate, stagger } from "framer-motion";
-import yeezyPhotos from "../../data/yeezyPhotos.js";
+import yeezyNews from "../../data/yeezyDayNews.js";
 import yeezyBG from "../../Images/yeezy_boots/blurryGradient.svg";
 import yeezyScrshot from "../../Images/yeezy_boots/blurry6.svg";
 
 function Yeezy() {
 
-    // const Modal = ({ isOpen, onClose, album }) => {
-  //   return (
-  //     <AnimatePresence>
-  //       {isOpen && (
-  //         <div>
-  //           <motion.div
-  //             initial={{
-  //               opacity: 0,
-  //             }}
-  //             animate={{
-  //               opacity: 1,
-  //               transition: {
-  //                 type: "spring",
-  //               },
-  //             }}
-  //             exit={{
-  //               opacity: 0,
-  //               transition: {
-  //                 delay: 0.3,
-  //               },
-  //             }}
-  //             onClick={onClose}
-  //             className="modal-backdrop"
-  //           />
-  //           <motion.div
-  //             initial={{
-  //               scale: 0,
-  //             }}
-  //             animate={{
-  //               scale: 1,
-  //               y: 200,
-  //               transition: {
-  //                 duration: 0.3,
-  //                 type: "spring",
-  //               },
-  //             }}
-  //             exit={{
-  //               scale: 0,
-  //               transition: {
-  //                 delay: 0.3,
-  //               },
-  //             }}
-  //             className="modal-content-wrapper"
-  //           >
-  //             <motion.div
-  //               initial={{
-  //                 opacity: 0,
-  //               }}
-  //               animate={{
-  //                 opacity: 1,
-  //                 transition: {
-  //                   delay: 0.2,
-  //                   duration: 3,
-  //                   type: "spring",
-  //                 },
-  //               }}
-  //               exit={{
-  //                 scale: 0,
-  //                 opacity: 0,
-  //                 transition: {
-  //                   duration: 0.7,
-  //                 },
-  //               }}
-  //               className="modal-content"
-  //             >
-  //               <div className="news_container">
-  //                 {/* <div> YEEZY DAY NEWS HERE </div> */}
-  //               </div>
-  //             </motion.div>
-  //           </motion.div>
-  //         </div>
-  //       )}
-  //     </AnimatePresence>
-  //   );
-  // };
+    const Modal = ({ isOpen, onClose, yeezyNews }) => {
+    return (
+      <AnimatePresence>
+        {isOpen && (
+          <div>
+            <motion.div
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  type: "spring",
+                },
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  delay: 0.3,
+                },
+              }}
+              onClick={onClose}
+              className="modal-backdrop"
+            />
+            <motion.div
+              initial={{
+                scale: 0,
+              }}
+              animate={{
+                scale: 1,
+                y: 200,
+                transition: {
+                  duration: 0.3,
+                  type: "spring",
+                },
+              }}
+              exit={{
+                scale: 0,
+                transition: {
+                  delay: 0.3,
+                },
+              }}
+              className="modal-content-wrapper"
+            >
+              <motion.div
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                    duration: 3,
+                    type: "spring",
+                  },
+                }}
+                exit={{
+                  scale: 0,
+                  opacity: 0,
+                  transition: {
+                    duration: 0.7,
+                  },
+                }}
+                className="modal-content"
+              >
+                <div className="news_container">
+                  Hello Yeezy
+                </div>
+              </motion.div>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    );
+  };
 
-  // const [modalOpen, setModalOpen] = useState(Array(albums.length).fill(false));
+  const [modalOpen, setModalOpen] = useState(Array(yeezyNews.length).fill(false));
 
-  // const openModal = (index) => {
-  //   setModalOpen(modalOpen.map((value, i) => (i === index ? true : value)));
-  // };
+  const openModal = (index) => {
+    setModalOpen(modalOpen.map((value, i) => (i === index ? true : value)));
+  };
 
-  // const closeModal = (index) => {
-  //   setModalOpen(modalOpen.map((value, i) => (i === index ? false : value)));
-  // };
+  const closeModal = (index) => {
+    setModalOpen(modalOpen.map((value, i) => (i === index ? false : value)));
+  };
 
   return (
     <>
@@ -106,21 +106,30 @@ function Yeezy() {
 
           <div className="yeezy_desc-wrapper">
             <h3 className="yeezy_desc">
-              Yeezy is a clothing company created by Kanye West in collaboration
-              with Adidas.
+              <h6 className="yzy_p yzy_p-1">Yeezy is Known for its futuristic approach to design,</h6><br/><h6 className="yzy_p yzy_p-2">Yeezy offers unique and innovative patterns that blend streetwear with elements of high fashion.</h6><br/> <h6 className="yzy_p yzy_p-3">Characterized by a minimalist style, refined color schemes, and the incorporation of advanced technologies.</h6>
             </h3>
           </div>
 
           <div className="yeezy_days-wrapper">
-            <div className="yeezy_day">
-
-            </div>
-            <div className="yeezy_day">
-
-            </div>
-            <div className="yeezy_day">
-
-            </div>
+          {yeezyNews.map((News, index) => (
+          <div key={News.id}>
+            <button
+              onClick={() => openModal(index)}
+              className={`modal-button-yzy${modalOpen[index] ? " hidden" : ""}`}
+            >
+              <img
+                src={News.imgSrc}
+                className="button_img-yzy"
+                alt={News.title}
+              ></img>
+            </button>
+            <Modal
+              isOpen={modalOpen[index]}
+              onClose={() => closeModal(index)}
+              album={News}
+            />
+          </div>
+        ))}
           </div>
 
           <div className="yeezy_line" />
---
+++ src/data/yeezyDayNews.js
@@ -0,0 +1,64 @@
+import foamrnrs_brown from "../Images/yeezy_boots/foamrunners_brown.jpg";
+import foamrnrs_grey from "../Images/yeezy_boots/foamrunners_grey.jpg";
+import foamrnrs_red from "../Images/yeezy_boots/foamrunners_red.jpg";
+import yzy350_ashstone from "../Images/yeezy_boots/yeezy350_ashstone.jpg";
+import yzy350_onyx from "../Images/yeezy_boots/yeezy350_onyx.jpg";
+import yzy350_white from "../Images/yeezy_boots/yeezy350_white.jpg";
+import yzy350_zebra from "../Images/yeezy_boots/yeezy350_zebra.jpg";
+import yzy450_cloudwhite from "../Images/yeezy_boots/yeezy450_cloudwhite.jpg";
+import yzy450_resin from "../Images/yeezy_boots/yeezy450_resin.jpg";
+import yzy500_blush from "../Images/yeezy_boots/yeezy500_blush.jpg";
+import yzy500_enflame from "../Images/yeezy_boots/yeezy500_enflame.jpg";
+import yzy500_granite from "../Images/yeezy_boots/yeezy500_granite.jpg";
+import yzy700_kyanite from "../Images/yeezy_boots/yeezy700_kyanite.jpg";
+import yzy700_waverunner from "../Images/yeezy_boots/yeezy700_waverunner.jpg";
+import yzyslide_core from "../Images/yeezy_boots/yeezyslide_core.jpg";
+import yzyslide_onyx from "../Images/yeezy_boots/yeezyslide_onyx.jpg";
+import yzyslide_resin from "../Images/yeezy_boots/yeezyslide_resin.jpg";
+
+const yeezyNews = [
+  {
+    id: 1,
+    imgClass: "yeezy_day-1",
+    imgSrc: yzy350_zebra,
+    titleClass: "yzy_d-title-1",
+    title: "YEEZY Day: 2009",
+    descClass: "yzy_d-desc-1",
+    desc: "This was the first Yeezy day that happened on 2009",
+    boots: [
+      "1. yeezy 350 zebra",
+      "2. yeezy 350 onyx",
+      "3. yeezy slide white",
+    ],
+  },
+  {
+    id: 2,
+    imgClass: "yeezy_day-2",
+    imgSrc: yzy350_ashstone,
+    titleClass: "yzy_d-title-2",
+    title: "YEEZY Day: 2009",
+    descClass: "yzy_d-desc-2",
+    desc: "This was the first Yeezy day that happened on 2009",
+    boots: [
+      "1. yeezy 350 zebra",
+      "2. yeezy 350 onyx",
+      "3. yeezy slide white",
+    ],
+  },
+  {
+    id: 3,
+    imgClass: "yeezy_day-3",
+    imgSrc: yzy350_onyx,
+    titleClass: "yzy_d-title-3",
+    title: "YEEZY Day: 2009",
+    descClass: "yzy_d-desc-3",
+    desc: "This was the first Yeezy day that happened on 2009",
+    boots: [
+      "1. yeezy 350 zebra",
+      "2. yeezy 350 onyx",
+      "3. yeezy slide white",
+    ],
+  }
+]
+
+export default yeezyNews;
