667ffe02bfd645ddc173e0b9a46d4d6716f0c055
MateuszKretkowski <mkretkowski13@gmail.com>
2023-12-17 23:41:55 +0100

    revamped yeezy

--- src/Components/yeezy/yeezy.tsx
+++ src/Components/yeezy/yeezy.tsx
@@ -35,7 +35,7 @@ function Yeezy_Boots( {start, end}: YeezyBootsProps) {
   const childVariant = {
     initial: {
       opacity: 0,
-      y: 100,
+      x: -100,
     },
     animate: (index: number) => ({
       opacity: 1,
@@ -66,31 +66,23 @@ function Yeezy_Boots( {start, end}: YeezyBootsProps) {
       {yeezyBoots.slice(start, end + 1).map((boots: any, index: number) => (
         <motion.div
           key={boots.id}
-          className="slider_img-wrapper"
+          className="boot-wrapper"
           variants={parentVariant}
           initial={{ y: randomOffsets[index] }}
           whileHover={{scale: 1.2}}
-          // onClick={() => setIsOpen(isOpen[index])}
           ref={ref}
         >
           <motion.img
             key={boots.id}
             src={boots.imgSrc}
-            className={"slider_image " + boots.imgClass}
+            className={boots.imgClass + " boot_img"}
+            style={{ zIndex: boots.random }}            
             variants={childVariant}
             initial="initial"
             whileInView={"animate"}
             custom={index}
             ref={inViewRef}
           />
-          {/* <motion.div className={"slider_image_desc-wrapper"}
-            animate={{
-              // opacity: isOpen[index] ? 1 : 0,
-            }}
-          >
-            <h3 className={"boots_title " + boots.titleClass}>{boots.title}</h3>
-            <h5 className={"boots_desc " + boots.descClass}>{boots.desc}</h5>
-          </motion.div> */}
         </motion.div>
       ))}
     </>
@@ -110,28 +102,17 @@ function Yeezy() {
 
   return (
     <AnimatePresence>
-      <motion.div className="yeezy" style={{ scale : scrollYProgress, y: scrollYProgress }}>
+      <motion.div className="yeezy" style={{ scaleY: scrollYProgress }}>
         <div className="yeezy_container">
           <div className="yeezy-wrapper">
-            <div className="yeezy_pboots_container">
+            <div className="title_wrapper">
               <h1>Yeezy Collections</h1>
               <button>Read More About Yeezy</button>
             </div>
           </div>
           <div className="yeezy_collections_container">
             <div className="collection-wrapper">
-              <div className="collection_slider-wrapper">
-                <div className="slider_row-1">
-                <Yeezy_Boots start={1} end={4} />
-                </div>
-                <div className="slider_row-1">
-                <Yeezy_Boots start={1} end={4} />
-                </div>
-              </div>
-              <div className="collection_text-wrapper">
-                <h2>Yeezy 350v2</h2>
-                <h5>350's Are the most popular sneaker collection all time!</h5>
-              </div>
+
             </div>
           </div>
         </div>
--- src/data/yeezyBoots.tsx
+++ src/data/yeezyBoots.tsx
@@ -1,139 +1,149 @@
-
 const foamrnrs_brown = require("../Images/yeezy_boots/foamrunners_brown.jpg");
 const foamrnrs_grey = require("../Images/yeezy_boots/foamrunners_grey.jpg");
 const foamrnrs_red = require("../Images/yeezy_boots/foamrunners_red.jpg");
 const yzy350_ashstone = require("../Images/yeezy_boots/yeezy350_ashstone.jpg");
-const yzy350_onyx = require("../Images/yeezy_boots/yeezy350_onyx.jpg");
-const yzy350_white = require("../Images/yeezy_boots/yeezy350_white.jpg");
-const yzy350_zebra = require("../Images/yeezy_boots/yeezy350_zebra.jpg");
+const yzy350_onyx = require("../Images/yeezy_boots/yeezy350_onyx.png");
+const yzy350_white = require("../Images/yeezy_boots/yeezy350_white.png");
+const yzy350_zebra = require("../Images/yeezy_boots/yeezy350_zebra.png");
 const yzy450_cloudwhite = require("../Images/yeezy_boots/yeezy450_cloudwhite.jpg");
 const yzy450_resin = require("../Images/yeezy_boots/yeezy450_resin.jpg");
 const yzy500_blush = require("../Images/yeezy_boots/yeezy500_blush.jpg");
 const yzy500_enflame = require("../Images/yeezy_boots/yeezy500_enflame.jpg");
 const yzy500_granite = require("../Images/yeezy_boots/yeezy500_granite.jpg");
 const yzy700_kyanite = require("../Images/yeezy_boots/yeezy700_kyanite.jpg");
-const yzy700_waverunner = require("../Images/yeezy_boots/yeezy700_waverunner.jpg");
-const yzyslide_core = require("../Images/yeezy_boots/yeezyslide_core.jpg");
-const yzyslide_onyx = require("../Images/yeezy_boots/yeezyslide_onyx.jpg");
-const yzyslide_resin = require("../Images/yeezy_boots/yeezyslide_resin.jpg");
 
 const yeezyBoots = [
   {
     id: 1,
-      imgClass: "yzy350_zebra",
-      imgSrc: yzy350_zebra,
-      titleClass: "yzy350_zebra_title",
-      title: "Yeezy",
-      descClass: "yzy350_zebra_desc",
-      desc: "Yeezy 350v2 Zebra is an incredible sneaker",
-    },
-    {
-      id: 2,
-      imgClass: "foamrnrs_brown",
-      imgSrc: foamrnrs_brown,
-      titleClass: "foamrnrs_brown_title",
-      title: "Foam Runners",
-      descClass: "foamrnrs_brown-desc",
-      desc: "Foam Runners in Brown are stylish and comfortable",
-    },
-    {
-      id: 3,
-      imgClass: "foamrnrs_grey",
-      imgSrc: foamrnrs_grey,
-      titleClass: "foamrnrs_grey_title",
-      title: "Foam Runners",
-      descClass: "foamrnrs_grey-desc",
-      desc: "Foam Runners in Grey are a perfect blend of fashion and function",
-    },
-    {
-      id: 4,
-      imgClass: "foamrnrs_red",
-      imgSrc: foamrnrs_red,
-      titleClass: "foamrnrs_red_title",
-      title: "Foam Runners",
-      descClass: "foamrnrs_red-desc",
-      desc: "Stand out with Foam Runners in Red, a bold choice for any outfit",
-    },
-    {
-      id: 5,
-      imgClass: "yzy350_ashstone",
-      imgSrc: yzy350_ashstone,
-      titleClass: "yzy350_ashstone_title",
-      title: "Yeezy",
-      descClass: "yzy350_ashstone-desc",
-      desc: "Yeezy 350v2 Ash Stone offers a unique and trendy look",
-    },
-    {
-      id: 6,
-      imgClass: "yzy350_onyx",
-      imgSrc: yzy350_onyx,
-      titleClass: "yzy350_onyx_title",
-      title: "Yeezy",
-      descClass: "yzy350_onyx-desc",
-      desc: "Yeezy 350v2 Onyx features a sleek and modern design",
-    },
-    {
-      id: 7,
-      imgClass: "yzy350_white",
-      imgSrc: yzy350_white,
-      titleClass: "yzy350_white_title",
-      title: "Yeezy",
-      descClass: "yzy350_white-desc",
-      desc: "Classic and clean, Yeezy 350v2 White is a timeless choice",
-    },
-    {
-      id: 8,
-      imgClass: "yzy450_cloudwhite",
-      imgSrc: yzy450_cloudwhite,
-      titleClass: "yzy450_cloudwhite_title",
-      title: "Yeezy",
-      descClass: "yzy450_cloudwhite-desc",
-      desc: "Yeezy 450 Cloud White offers futuristic style and comfort",
-    },
-    {
-      id: 9,
-      imgClass: "yzy450_resin",
-      imgSrc: yzy450_resin,
-      titleClass: "yzy450_resin_title",
-      title: "Yeezy",
-      descClass: "yzy450_resin-desc",
-      desc: "Yeezy 450 Resin showcases bold and innovative design",
-    },
-    {
-      id: 10,
-      imgClass: "yzy500_blush",
-      imgSrc: yzy500_blush,
-      titleClass: "yzy500_blush_title",
-      title: "Yeezy",
-      descClass: "yzy500_blush-desc",
-      desc: "Yeezy 500 Blush combines style and comfort effortlessly",
-    },
-    {
-      id: 11,
-      imgClass: "yzy500_enflame",
-      imgSrc: yzy500_enflame,
-      titleClass: "yzy500_enflame_title",
-      title: "Yeezy",
-      descClass: "yzy500_enflame-desc",
-      desc: "Yeezy 500 Enflame features a vibrant and eye-catching design",
-    },
-    {
-      id: 12,
-      imgClass: "yzy500_granite",
-      imgSrc: yzy500_granite,
-      titleClass: "yzy500_granite_title",
-      title: "Yeezy",
-      descClass: "yzy500_granite-desc",
-      desc: "Yeezy 500 Granite offers a rugged and stylish aesthetic",
-    },
-    {
-      id: 13,
-      imgClass: "yzy700_kyanite",
-      imgSrc: yzy700_kyanite,
-      titleClass: "yzy700_kyanite_title",
-      title: "Yeezy",
-      descClass: "yzy700_kyanite-desc",
-      desc: "Yeezy 700 Kyanite delivers a bold and dynamic look",
-    },]
-        
-        export default yeezyBoots;
+    imgClass: "yzy350_zebra",
+    imgSrc: yzy350_zebra,
+    titleClass: "yzy350_zebra_title",
+    title: "Yeezy",
+    descClass: "yzy350_zebra_desc",
+    desc: "Yeezy 350v2 Zebra is an incredible sneaker",
+    z: "1",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 2,
+    imgClass: "foamrnrs_brown",
+    imgSrc: foamrnrs_brown,
+    titleClass: "foamrnrs_brown_title",
+    title: "Foam Runners",
+    descClass: "foamrnrs_brown-desc",
+    desc: "Foam Runners in Brown are stylish and comfortable",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 3,
+    imgClass: "foamrnrs_grey",
+    imgSrc: foamrnrs_grey,
+    titleClass: "foamrnrs_grey_title",
+    title: "Foam Runners",
+    descClass: "foamrnrs_grey-desc",
+    desc: "Foam Runners in Grey are a perfect blend of fashion and function",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 4,
+    imgClass: "foamrnrs_red",
+    imgSrc: foamrnrs_red,
+    titleClass: "foamrnrs_red_title",
+    title: "Foam Runners",
+    descClass: "foamrnrs_red-desc",
+    desc: "Stand out with Foam Runners in Red, a bold choice for any outfit",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 5,
+    imgClass: "yzy350_ashstone",
+    imgSrc: yzy350_ashstone,
+    titleClass: "yzy350_ashstone_title",
+    title: "Yeezy",
+    descClass: "yzy350_ashstone-desc",
+    desc: "Yeezy 350v2 Ash Stone offers a unique and trendy look",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 6,
+    imgClass: "yzy350_onyx",
+    imgSrc: yzy350_onyx,
+    titleClass: "yzy350_onyx_title",
+    title: "Yeezy",
+    descClass: "yzy350_onyx-desc",
+    desc: "Yeezy 350v2 Onyx features a sleek and modern design",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 7,
+    imgClass: "yzy350_white",
+    imgSrc: yzy350_white,
+    titleClass: "yzy350_white_title",
+    title: "Yeezy",
+    descClass: "yzy350_white-desc",
+    desc: "Classic and clean, Yeezy 350v2 White is a timeless choice",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 8,
+    imgClass: "yzy450_cloudwhite",
+    imgSrc: yzy450_cloudwhite,
+    titleClass: "yzy450_cloudwhite_title",
+    title: "Yeezy",
+    descClass: "yzy450_cloudwhite-desc",
+    desc: "Yeezy 450 Cloud White offers futuristic style and comfort",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 9,
+    imgClass: "yzy450_resin",
+    imgSrc: yzy450_resin,
+    titleClass: "yzy450_resin_title",
+    title: "Yeezy",
+    descClass: "yzy450_resin-desc",
+    desc: "Yeezy 450 Resin showcases bold and innovative design",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 10,
+    imgClass: "yzy500_blush",
+    imgSrc: yzy500_blush,
+    titleClass: "yzy500_blush_title",
+    title: "Yeezy",
+    descClass: "yzy500_blush-desc",
+    desc: "Yeezy 500 Blush combines style and comfort effortlessly",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 11,
+    imgClass: "yzy500_enflame",
+    imgSrc: yzy500_enflame,
+    titleClass: "yzy500_enflame_title",
+    title: "Yeezy",
+    descClass: "yzy500_enflame-desc",
+    desc: "Yeezy 500 Enflame features a vibrant and eye-catching design",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 12,
+    imgClass: "yzy500_granite",
+    imgSrc: yzy500_granite,
+    titleClass: "yzy500_granite_title",
+    title: "Yeezy",
+    descClass: "yzy500_granite-desc",
+    desc: "Yeezy 500 Granite offers a rugged and stylish aesthetic",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+  {
+    id: 13,
+    imgClass: "yzy700_kyanite",
+    imgSrc: yzy700_kyanite,
+    titleClass: "yzy700_kyanite_title",
+    title: "Yeezy",
+    descClass: "yzy700_kyanite-desc",
+    desc: "Yeezy 700 Kyanite delivers a bold and dynamic look",
+    random: Math.floor(Math.random() * 10) + 1,
+  },
+];
+
+export default yeezyBoots;
